3bf84aa1584c9b85ca1f02dc0021fe5b08aa9e43
Musaul <musaulkarim@tengroup.com>
2020-05-17 00:59:24 +0100

    S3 Setup

--- s3/create_and_popluate_fe_s3.sh
+++ s3/create_and_popluate_fe_s3.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
 DOMAIN=$1
-SRC_S3_BUCKET=$2
+ENV=$2
 
-# generate policy file from template
+SRC_S3_BUCKET=$3
+
+REGION="eu-west-1"
+WEBSITE_CFG="artifacts/bucket-websites.cfg.json"
+POLICY_TEMPLATE="templates/bucket-policy.template.json"
+POLICY_CFG="scratch/bucket-policy.cfg.json"
 
 
+cd "$(dirname "$0")"
+mkdir -p scratch
+
+echo
+echo "Creating Bucket for '${DOMAIN}' in '${REGION}'  ..."
+aws s3api create-bucket  --bucket ${DOMAIN}  --create-bucket-configuration LocationConstraint=${REGION}  --region ${REGION}  --profile ${ENV}
+
+echo
+echo "Enabling Bucket Website  ..."
+aws s3api put-bucket-website  --bucket ${DOMAIN}  --website-configuration file://${WEBSITE_CFG}  --profile ${ENV}
+
+echo
+echo "Generating Bucket Policy file from template  ..."
+cp -v  ${POLICY_TEMPLATE}  ${POLICY_CFG}
+sed -i  "s/\${DOMAIN}/${DOMAIN}/g"  ${POLICY_CFG}
+
+echo
+echo "Setting Bucket Policy  ..."
+aws s3api put-bucket-policy  --bucket ${DOMAIN}  --policy file://${POLICY_CFG}  --profile ${ENV}
+
+echo
+echo DONE
